28df433b6bf7d0a2a78b59c71f6eab424d8116fc
Tomasz Koszarek <402524@student.agh.edu.pl>
2022-01-17 11:57:48 +0100

    Pull request #30: - fixed teleport
    Merge in TO/sr-1615-it-toads from refactor to master
    
    Squashed commit of the following:
    
    commit 7c2517eebcf9c3ce92cc32ed319e8e4dda97a7b0
    Author: Tomasz Koszarek <402524@student.agh.edu.pl>
    Date:   Mon Jan 17 11:55:23 2022 +0100
    
        - fixed teleport

--- src/main/java/model/entity/Teleport.java
+++ src/main/java/model/entity/Teleport.java
@@ -1,9 +1,17 @@
 package model.entity;
 
+import model.Grid;
 import utils.EntityVisitor;
 import utils.Vector2d;
 
-public class Teleport extends Entity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Teleport extends PowerUpEntity {
 
     public Teleport(int x, int y) {
         super(x, y);
@@ -16,5 +24,16 @@ public class Teleport extends Entity {
     @Override
     public void accept(EntityVisitor visitor) {
         visitor.visit(this);
+    }
+
+    public void use(Grid grid){
+        var height = grid.getHeight();
+        var width = grid.getWidth();
+        List<Vector2d> freePositions = IntStream
+                .rangeClosed(0, width * height - 1)
+                .mapToObj(i -> new Vector2d(i % width, i / width))
+                .collect(Collectors.toCollection(ArrayList::new));
+        Collections.shuffle(freePositions, new Random(System.currentTimeMillis()));
+        grid.getDoctor().teleport(freePositions.get(0));
     }
 }
